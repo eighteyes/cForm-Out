9301f81423cf904f1def4eb392b5b6a88605f582
Sean Canton <sean@8isc.com>
2012-04-02 21:47:30 -0500

    Simple Changes + README

--- cformout.php
+++ cformout.php
@@ -11,17 +11,16 @@ License: Fark
 
 function printForm( $atts ){
 	global $wpdb;
-	
+
 	extract (shortcode_atts( array('id' => '1'), $atts ));
-	
-	//return "test";
+
 	$sql = "SELECT field_name,field_val FROM wp_cformsdata
 	WHERE sub_id = $id";
-	
+
 	$data = $wpdb->get_results($sql, ARRAY_N);
-	
+
 	$html = "<style>.cf-out label {width: 100px;font-size: 12px; text-align:right; display: inline-block;padding-right:10px; } #cFormOut{float:left;}</style>";
-	
+
 	if ($data) {
 		$i = 0;
 		foreach ($data as $item) {
@@ -29,11 +28,11 @@ function printForm( $atts ){
 			if ($item[0] == 'page'){
 				$title = trim($item[1], '/');
 				$title = str_replace('-', ' ', $title);
-				
+
 				$html .= "<h1>".  ucwords($title) . "</h1>";
 			} else {
 				$html .= "<p class='cf-out'><label>$item[0]</label>";
-			
+
 				if (strpos($item[0], 'Upload') > 0) {
 					$html .= "<img src='".plugins_url()."/cforms/uploads/". $id ."-". $item[1] ."'>";
 				} else {
@@ -42,7 +41,7 @@ function printForm( $atts ){
 			}
 		}
 	}
-	
+
 	return "<div id='cFormOut'>" . $html . "</div>";
 }
 
